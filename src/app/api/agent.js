3427b33840a59de01892e614a85079211333cfee
NhonVo <vothuongtruongnhon2002@gmail.com>
2023-07-20 14:17:39 +0700

    update host url

--- src/app/api/agent.js
+++ src/app/api/agent.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:5046'; // Replace with your actual base URL
+const baseURL = 'https://portfolio-truongnhon.azurewebsites.net/'; // Replace with your actual base URL
 
 const agent = axios.create({
   baseURL,
