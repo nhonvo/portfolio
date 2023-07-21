6ea7d7e51aced419bd2ddc6d7d495673758ed2ed
NhonVo <vothuongtruongnhon2002@gmail.com>
2023-07-21 21:18:49 +0700

    update html render

--- src/features/Home/project/Project.js
+++ src/features/Home/project/Project.js
@@ -25,9 +25,11 @@ function Project() {
                             <a key={index} href={item?.linkProject} target="_blank" className="work__img" title="" rel="noopener noreferrer">
                                 <img src={item?.imageURL} alt="" style={{ padding: '1rem' }} />
                                 <h2 className="about__subtitle" style={{ color: 'rgb(49, 112, 238)', padding: '1rem' }}>{item?.name}</h2>
-                                <p className="about__text" style={{ color: 'black', padding: '0 0 0.5rem 0.5rem' }}>
-                                    {item?.description}
-                                </p>
+                                <div
+                                    className="about__text"
+                                    style={{ color: 'black', padding: '0 0 0.5rem 0.5rem' }}
+                                    dangerouslySetInnerHTML={{ __html: item?.description }}
+                                ></div>
                             </a>
                         ))
                     }
